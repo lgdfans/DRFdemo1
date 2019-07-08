6b8da46e8f8cd8131efdc5e96d2fc929684d00e2
lgdfans <343396447@qq.com>
2019-07-08 19:55:31 +0800

    修改测试test.py文件

--- stuapp/tests.py
+++ stuapp/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+class MinStack:
 
-# Create your tests here.
+    def __init__(self):
+        # do intialization if necessary
+        self.stack = []
+
+    """
+    @param: number: An integer
+    @return: nothing
+    """
+
+    def push(self, number):
+        # write your code here
+        self.stack.append(number)
+
+    """
+    @return: An integer
+    """
+
+    def pop(self):
+        # write your code here
+        n = self.stack[-1]
+        self.stack = self.stack[0:len(self.stack)]
+        return n
+
+    """
+    @return: An integer
+    """
+
+    def min(self):
+        # write your code here
+        n = self.stack[0]
+        for v in self.stack:
+            if v < n:
+                n = v
+        return n
+
+
+
+
